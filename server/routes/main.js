9eecc559f50f24a8f11b912e327481d4c608163c
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-04-21 13:23:03 -0400

    Added post.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,15 +3,82 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 
-//Routes
-router.get('',(req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Blog powered by NodeJs, Express & MongoDB."
-  }
 
-  res.render('index', { locals }); //rendering home page
+
+
+//Pagination
+router.get('', async (req, res) => {
+try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Blog powered by NodeJs, Express & MongoDB."
+    }
+
+    let perPage = 10; //how many it displays at a time
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: {createdAt: -1} }])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+  
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count/perPage);
+
+  res.render('index', { 
+    locals, 
+    data,
+    current: page,
+    nextPage: hasNextPage ? nextPage : null
+  }); 
+
+} catch (error) {
+  console.log(error);
+}
+
 });   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//GET Post :id
+router.get('/post/:id', async (req, res) => {
+  try {
+
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Blog powered by NodeJs, Express & MongoDB."
+    }
+
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+res.render('post', { locals, data});
+} catch (error) {
+  console.log(error);
+}
+
+});
+
+
+
 
 router.get('/about',(req, res) => {
   res.render('about'); 
@@ -21,50 +88,9 @@ router.get('/contact',(req, res) => {
   res.render('contact'); 
 }); 
 
-function insertPostData () {
-  Post.insertMany([
-    {
-            title: "Building APIs with Node.js",
-            body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
-          },
-          {
-            title: "Deployment of Node.js applications",
-            body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
-          },
-          {
-            title: "Authentication and Authorization in Node.js",
-            body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
-          },
-          {
-            title: "Understand how to work with MongoDB and Mongoose",
-            body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
-          },
-          {
-            title: "build real-time, event-driven applications in Node.js",
-            body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
-          },
-          {
-            title: "Discover how to use Express.js",
-            body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
-          },
-          {
-            title: "Asynchronous Programming with Node.js",
-            body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
-          },
-          {
-            title: "Learn the basics of Node.js and its architecture",
-            body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
-          },
-          {
-            title: "NodeJs Limiting Network Traffic",
-            body: "Learn how to limit netowrk traffic."
-          },
-          {
-            title: "Learn Morgan - HTTP Request logger for NodeJs",
-            body: "Learn Morgan."
-          },
-])
-}
-insertPostData();
+
+
+
+
 
 module.exports = router;
